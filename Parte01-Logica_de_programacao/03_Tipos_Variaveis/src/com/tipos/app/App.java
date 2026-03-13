d3ffc47d87b47414081d16f8840069034698fb11
joice003 <joicemonteirodasilva@gmail.com>
2026-03-13 20:59:25 -0300

    Primeiro commit.

---
+++ Parte01-Logica_de_programacao/03_Tipos_Variaveis/src/com/tipos/app/App.java
@@ -0,0 +1,15 @@
+package com.tipos.app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // declarar variables
+        String name = "Joice Monteiro";
+        int age = 18;
+        double height = 1.72;
+        //saida de dados
+        System.out.println("Name: " + name + ".");
+        System.out.println("Age: " + age + ".");
+        System.out.println("Height: " + height + " meters.");   
+        
+    }
+}
