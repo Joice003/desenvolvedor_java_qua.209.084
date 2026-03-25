18e69f65c934e58812fb4d203401c77372b44eb6
ead <ead@academico.local>
2026-03-24 21:43:29 -0300

    Aula do dia 24/03/2026.

---
+++ Atividades/atividade_04/src/com/atividade04/app/App.java
@@ -0,0 +1,40 @@
+package com.atividade04.app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // intancia a classe Veiculo
+        Veiculo veiculo = new Veiculo();
+        Scanner sc = new Scanner(System.in);
+
+        // entrada de dados
+        System.out.println("Informe o Fabricante do veículo:");
+        veiculo.fabricante = sc.nextLine();
+        System.out.println("Informe o Modelo do veículo:");
+        veiculo.modelo = sc.nextLine();
+        System.out.println("Informe a Placa do veículo:");
+        veiculo.placa = sc.nextLine();
+        System.out.println("Informe o Ano do veículo:");
+        veiculo.ano = sc.nextInt();
+        System.out.println("Informe a cor do veículo:");
+        veiculo.cor = sc.nextLine();
+
+        // define os valores dos atributos
+        veiculo.fabricante = "Fiat";
+        veiculo.modelo = "Mobi";
+        veiculo.placa = "A7B2KSE";
+        veiculo.ano = 2019;
+        veiculo.cor = "Azul";
+
+        // saida de dados
+        System.out.println("Atributos do objeto:");
+        System.out.println("Fabricante: " + veiculo.fabricante);
+        System.out.println("Modelo: " + veiculo.modelo);
+        System.out.println("Placa: " + veiculo.placa);
+        System.out.println("Ano: " + veiculo.ano);
+        System.out.println("Cor: " + veiculo.cor);
+
+        sc.close();
+    }
+}
---
+++ Atividades/atividade_04/src/com/atividade04/app/Veiculo.java
@@ -0,0 +1,12 @@
+package com.atividade04.app;
+
+public class Veiculo {
+// atributos
+public String fabricante;
+public String modelo;
+public String placa;
+public int ano;
+public String cor;
+
+}
+
