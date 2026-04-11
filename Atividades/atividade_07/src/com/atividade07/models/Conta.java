c5671bd6ab7d7295b00d097de01725415a98af9a
ead <vitor.i.lopes@aluno.senai.br>
2026-04-10 21:42:22 -0300

    Aula do dia 10/04/2026.

--- Atividades/atividade_07/src/com/atividade07/models/Conta.java
+++ Atividades/atividade_07/src/com/atividade07/models/Conta.java
@@ -1,5 +1,7 @@
 package com.atividade07.models;
 
+import com.atividade07.repository.IConta;
+
 public class Conta {
 
 }
--- Atividades/atividade_07/src/com/atividade07/models/ContaJuridica.java
+++ Atividades/atividade_07/src/com/atividade07/models/ContaJuridica.java
@@ -1,5 +1,49 @@
 package com.atividade07.models;
 
-public class ContaJuridica {
+public class ContaJuridica extends Conta {
+    private PessoaJuridica pessoaJuridica;
+    private double taxa = 0.01;
+
+    public ContaJuridica(PessoaJuridica pessoaJuridica, double taxa, String conta, String agencia, double saldo) {
+        super(conta, agencia, saldo);
+        this.pessoaJuridica = pessoaJuridica;
+        this.taxa;    
+    }
+
+
+    public PessoaJuridica getPessoaJuridica() {
+        return this.pessoaJuridica;
+    }
+
+    public void setPessoaJuridica(PessoaJuridica pessoaJuridica) {
+        this.pessoaJuridica = pessoaJuridica;
+    }
+
+    public double getTaxa() {
+        return this.taxa;
+    }
+
+    public void setTaxa(double taxa) {
+        this.taxa = taxa;
+    }
+
+
+    public void exibirDados() {
+        System.out.println("Razão social da empresa: " + this.pessoaJuridica.getRazaoSocial());
+        System.out.println("Nome Fantasia da empresa: " + this.pessoaJuridica.getNomeFantasia());
+        System.out.println("CNPJ da empresa: " + this.pessoaJuridica.getCnpj());
+        System.out.println("E=mail da empresa: " + this.pessoaJuridica.getEmail());
+        super.exibirDados();       
+    }
+
+    public double fazerSaque(double valor) {
+        this.setSaldo(this.getSaldo() - valor - (this.getSaldo() - 0.01/100));
+        return getSaldo();
+    }
+
+
+
+    }
+    
 
 }
